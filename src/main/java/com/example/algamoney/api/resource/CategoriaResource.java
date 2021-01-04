b894040f94973761debf2edaca90c487c794c551
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-01-04 15:03:45 -0300

    Implementando o 404 na busca de categorias - Aula 3.7

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -42,8 +42,11 @@ public class CategoriaResource {
 	}
 	
 	@GetMapping("/{codigo}")
-	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
-		return categoriaRepository.findOne(codigo);
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo, HttpServletResponse response) {
+		
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
 
 }
