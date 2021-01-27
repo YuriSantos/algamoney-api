61f3b76ad58c2f061ea29ffbd8e918ea30cc3f77
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-01-27 13:45:56 -0300

    Adicionando o salvar lançamento - Aula 5.3

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -5,9 +5,12 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,14 @@ public class LancamentoResource {
 	
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@RequestBody Lancamento lancamento,
+			HttpServletResponse response){
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
 	
 	@GetMapping
 	public List<Lancamento> listar(){
---
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -0,0 +1,12 @@
+package com.example.algamoney.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.algamoney.api.repository.LancamentoRepository;
+
+public class LancamentoService {
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+}
