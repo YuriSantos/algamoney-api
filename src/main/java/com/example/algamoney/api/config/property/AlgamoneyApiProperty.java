3c6fe76b0505ffa233732357f89a6bc0215c3c84
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-02-01 21:38:06 -0300

    Deploy da API na nuvem - Aula 7.4

--- src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -5,12 +5,22 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
 	
+	private String originPermitida = "http://localhost:8080"
+	
 	private final Seguranca seguranca = new Seguranca();
 	
 	public Seguranca getSeguranca() {
 		return seguranca;
 	}
 	
+	public String getOriginPermitida() {
+		return originPermitida;
+	}
+
+	public void setOriginPermitida(String originPermitida) {
+		this.originPermitida = originPermitida;
+	}
+
 	public static class Seguranca{
 		
 		private boolean enableHttps;
